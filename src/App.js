e536c7871d4dc9836fe1bd1d70bad916c10efb5e
Vikki <vbro1293@gmail.com>
2024-11-08 17:17:21 +0000

    Implement and log field change handler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import WebViewer from '@pdftron/webviewer';
 import './App.css';
 
 const App = () => {
   const viewer = useRef(null);
+  const webviewerInstance = useRef();
+  const [webviewerInstanceExists, setWebviewerInstanceExists] = useState(false);
 
   // if using a class, equivalent of componentDidMount 
   useEffect(() => {
@@ -11,30 +13,63 @@ const App = () => {
     WebViewer.WebComponent(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
+        initialDoc: '/files/checkbox-radio-test.pdf',
         licenseKey: 'your_license_key',  // sign up to get a free trial key at https://dev.apryse.com
       },
       viewer.current,
     ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
-
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
-
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
-      });
+      webviewerInstance.current = instance;
+      setWebviewerInstanceExists(true);
     });
   }, []);
+
+  const onFormFieldChanged = (name, value) => {
+    console.log('Field changed:', name, value);
+  }
+
+
+  useEffect(() => {
+    const handleFieldChanged = async (firstarg, value) => {
+      const name = firstarg.name;
+      console.log(">>>>>>> ~ handleFieldChanged ~ name:", name)
+
+      const type = firstarg.type;
+      console.log(">>>>>>> ~ handleFieldChanged ~ type:", type)
+      
+      const flags = firstarg.flags;
+      // console.log(">>>>>>> ~ handleFieldChanged ~ flags:", flags)
+      const isRadio = flags?.get('Radio');
+      console.log(">>>>>>>>> isRadio", isRadio)
+
+      let valueToSave = value;
+
+      if (type === 'Btn' && !isRadio) {
+        valueToSave = value !== 'Off';
+      }
+      console.log(">>>>>>> ~ handleFieldChanged ~ valueToSave:", valueToSave)
+
+      onFormFieldChanged?.({ name, value: valueToSave });
+    };
+
+    if (webviewerInstance.current && onFormFieldChanged) {
+      const instance = webviewerInstance.current;
+      instance.Core.annotationManager.addEventListener(
+        'fieldChanged',
+        handleFieldChanged
+      );
+
+    }
+    return () => {
+      if (webviewerInstance.current) {
+        const instance = webviewerInstance.current;
+        instance.Core.annotationManager.removeEventListener(
+          'fieldChanged',
+          handleFieldChanged
+        );
+
+      }
+    };
+  }, [onFormFieldChanged, webviewerInstanceExists]);
 
   return (
     <div className="App">
